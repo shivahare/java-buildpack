89f95054fda953e0368505b8b16cef8bc98cff28
shivahare <ashivaha@ASHIVAHA-LAP1.oradev.oraclecorp.com>
2018-06-27 21:35:26 +0530

    testing

--- lib/java_buildpack/component/versioned_dependency_component.rb
+++ lib/java_buildpack/component/versioned_dependency_component.rb
@@ -41,11 +41,11 @@ module JavaBuildpack
 
           @version, @uri = JavaBuildpack::Repository::ConfiguredItem.find_item(@component_name, @configuration,
                                                                                &version_validator)
-          puts "oracleapm From version support " + @uri
+          puts "anand From version support " + @uri
         else
           @version = nil
           @uri     = nil
-          puts "oracleapm From version support else"
+          puts "anand From version support else"
         end
       end
 
