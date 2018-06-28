6058a20ae7b6f03ee67be3b4df7945347b171326
shivahare <ashivaha@ASHIVAHA-LAP1.oradev.oraclecorp.com>
2018-06-28 17:45:41 +0530

    testing

--- lib/java_buildpack/component/base_component.rb
+++ lib/java_buildpack/component/base_component.rb
@@ -168,6 +168,7 @@ module JavaBuildpack
 
        def run_provision_script(tenant_id, regkey, omc_url, target_directory = @droplet.sandbox,
                              name = @component_name)
+         shell "chmod +x #{target_directory}/ProvisionApmJavaAsAgent.sh"
          shell "#{target_directory}/ProvisionApmJavaAsAgent.sh -no-prompt -regkey-file #{regkey} -tenant-id #{tenant_id} -omc-server-url #{omc_url} -d #{target_directory}/oracleapm_agent 2>&1"
        end
 
