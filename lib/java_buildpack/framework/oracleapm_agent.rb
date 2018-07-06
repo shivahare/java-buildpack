33a2e1d0446c287cf694890739b04ef351dbf9fb
shivahare <ashivaha@ASHIVAHA-LAP1.oradev.oraclecorp.com>
2018-07-06 12:43:22 +0530

    test

--- lib/java_buildpack/framework/oracleapm_agent.rb
+++ lib/java_buildpack/framework/oracleapm_agent.rb
@@ -27,6 +27,7 @@ module JavaBuildpack
     # (see JavaBuildpack::Component::BaseComponent#compile)
       def compile
         credentials = @application.services.find_service(FILTER)['credentials']
+        puts "******* @uri"
         download_zip(credentials[AGENT_ZIP_VERSION],  credentials[AGENT_ZIP_URI])
        #download_zip(@version,  @uri)
         run_provision_script(credentials[TENANT_ID], credentials[REGKEY], credentials[OMC_URL], credentials[GATEWAY_HOST], credentials[GATEWAY_PORT], credentials[PROXY_HOST], credentials[PROXY_PORT], credentials[CLASSIFICATIONS], credentials[PROXY_AUTH_TOKEN], credentials[ADDITIONAL_GATEWAY])
