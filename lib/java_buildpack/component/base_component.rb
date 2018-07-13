db13457f55f3d77254bc7a1459fa84e2c7b983a3
shivahare <ashivaha@ASHIVAHA-LAP1.oradev.oraclecorp.com>
2018-07-13 12:51:14 +0530

    testing

--- lib/java_buildpack/component/base_component.rb
+++ lib/java_buildpack/component/base_component.rb
@@ -184,7 +184,8 @@ module JavaBuildpack
          puts "java_home : #{@droplet.java_home.root}"
 
          provision_cmd = StringIO.new
-         provision_cmd << "#{target_directory}/ProvisionApmJavaAsAgent.sh -regkey #{regkey} -no-wallet -d #{target_directory} -exact-hostname -no-prompt -tenant-id  #{tenant_id} -java-home #{@droplet.java_home.root} "
+         #provision_cmd << "#{target_directory}/ProvisionApmJavaAsAgent.sh -regkey #{regkey} -no-wallet -d #{target_directory} -exact-hostname -no-prompt -tenant-id  #{tenant_id} -java-home #{@droplet.java_home.root} "
+         provision_cmd << "#{target_directory}/ProvisionApmJavaAsAgent.sh -regkey #{regkey} -no-wallet -d #{target_directory} -exact-hostname -no-prompt -tenant-id  #{tenant_id} "
          if not_blank?(omc_url)
            provision_cmd << " -omc-server-url #{omc_url}"
          end
@@ -215,6 +216,11 @@ module JavaBuildpack
 
          Dir.chdir target_directory do
            #shell "#{target_directory}/ProvisionApmJavaAsAgent.sh -regkey #{regkey} -no-wallet -ph #{proxy_host} -d #{target_directory} -exact-hostname -no-prompt -omc-server-url #{omc_url} -tenant-id  #{tenant_id} -java-home #{@droplet.java_home.root} 2>&1"
+
+           myJavaHome="JAVA_HOME=#{@droplet.java_home.root}"
+           puts " java : #{myJavaHome}"
+           shell "echo "#{myJavaHome}" > ProvisionApmJavaAsAgent_CF.sh
+           shell "cat ProvisionApmJavaAsAgent.sh >> ProvisionApmJavaAsAgent_CF.sh"
            shell "#{provision_cmd.string}"
          end
        end
