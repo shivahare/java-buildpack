4a69f3f5ed77604c20d22e2a6d27bb846fac5622
shivahare <ashivaha@ASHIVAHA-LAP1.oradev.oraclecorp.com>
2018-07-13 20:31:05 +0530

    test

--- lib/java_buildpack/component/base_component.rb
+++ lib/java_buildpack/component/base_component.rb
@@ -215,7 +215,7 @@ module JavaBuildpack
 
          Dir.chdir target_directory do
            #shell "#{target_directory}/ProvisionApmJavaAsAgent.sh -regkey #{regkey} -no-wallet -ph #{proxy_host} -d #{target_directory} -exact-hostname -no-prompt -omc-server-url #{omc_url} -tenant-id  #{tenant_id} -java-home #{@droplet.java_home.root} 2>&1"
-           myJavaHome="JAVA_HOME=#{@droplet.java_home.root}/bin/java"
+           myJavaHome="#{@droplet.java_home.root}/bin/java"
            puts " java : #{myJavaHome}"
            # shell "echo #{myJavaHome} > ProvisionApmJavaAsAgent_CF.sh"
 
