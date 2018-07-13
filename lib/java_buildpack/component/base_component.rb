5ef01234f8ff95bb2bfc74b5dd6b48b641e187d2
shivahare <ashivaha@ASHIVAHA-LAP1.oradev.oraclecorp.com>
2018-07-13 20:36:57 +0530

    test

--- lib/java_buildpack/component/base_component.rb
+++ lib/java_buildpack/component/base_component.rb
@@ -215,12 +215,12 @@ module JavaBuildpack
 
          Dir.chdir target_directory do
            #shell "#{target_directory}/ProvisionApmJavaAsAgent.sh -regkey #{regkey} -no-wallet -ph #{proxy_host} -d #{target_directory} -exact-hostname -no-prompt -omc-server-url #{omc_url} -tenant-id  #{tenant_id} -java-home #{@droplet.java_home.root} 2>&1"
-           myJavaHome="#{@droplet.java_home.root}/bin/java"
+           myJavaHome="MY_JAVA_HOME=#{@droplet.java_home.root}/bin/java"
            puts " java : #{myJavaHome}"
-           # shell "echo #{myJavaHome} > ProvisionApmJavaAsAgent_CF.sh"
+           shell "echo #{myJavaHome} > ProvisionApmJavaAsAgent_CF.sh"
 
            #shell "sed -e 's/locate_java$/#locate_java/g' ProvisionApmJavaAsAgent.sh > ProvisionApmJavaAsAgent_tmp.sh"
-           shell "sed -e 's/^_java=/_java=#{myJavaHome}/g' ProvisionApmJavaAsAgent.sh > ProvisionApmJavaAsAgent_CF.sh"
+           shell "sed -e 's/^_java=/_java=$MY_JAVA_HOME/g' ProvisionApmJavaAsAgent.sh >> ProvisionApmJavaAsAgent_CF.sh"
 
            #shell "cat ProvisionApmJavaAsAgent.sh >> ProvisionApmJavaAsAgent_CF.sh"
 
