539e295faac55fa9a1895827297dcb1408a7b100
shivahare <ashivaha@ASHIVAHA-LAP1.oradev.oraclecorp.com>
2018-07-16 16:14:26 +0530

    test

--- lib/java_buildpack/framework/oracleapm_agent.rb
+++ lib/java_buildpack/framework/oracleapm_agent.rb
@@ -29,7 +29,7 @@ module JavaBuildpack
       # @param [Hash] context a collection of utilities used the component
       def initialize(context)
         super(context)
-        @version, @uri = ['latest', 'https://check-app-ok-2.cfapps.io/apm-agents/apmagent.zip']
+        #@version, @uri = ['latest', 'https://check-app-ok-2.cfapps.io/apm-agents/apmagent.zip']
       end
 
     # (see JavaBuildpack::Component::BaseComponent#compile)
