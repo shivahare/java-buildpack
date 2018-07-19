53c77f96b16777a521d57962a116573f603aafa2
shivahare <ashivaha@ASHIVAHA-LAP1.oradev.oraclecorp.com>
2018-07-19 13:55:42 +0530

    added more params

--- lib/java_buildpack/framework/oracleapm_agent.rb
+++ lib/java_buildpack/framework/oracleapm_agent.rb
@@ -54,12 +54,12 @@ module JavaBuildpack
         # Run apm provisioning script to install agent
         run_apm_provision_script(tenantId, regKey, omcUrl, gatewayH, gatewayP, credentials[PROXY_HOST], credentials[PROXY_PORT],
                                  credentials[CLASSIFICATIONS], credentials[PROXY_AUTH_TOKEN], credentials[ADDITIONAL_GATEWAY],
-                                 credentials[V], credentials[DEBUG], credentials[INSECURE])
+                                 credentials[V], credentials[DEBUG], credentials[INSECURE], , credentials[H])
       end
 
 
       def run_apm_provision_script(tenant_id, regkey, omc_url, gateway_host, gateway_port, proxy_host, proxy_port,
-                                   classifications, proxy_auth_token, additional_gateway, v, debug, insecure,
+                                   classifications, proxy_auth_token, additional_gateway, v, debug, insecure, hostname,
                                    target_directory = @droplet.sandbox,
                                    name = @component_name)
        shell "chmod +x #{target_directory}/ProvisionApmJavaAsAgent.sh"
@@ -76,6 +76,7 @@ module JavaBuildpack
        puts "additional_gateway : #{additional_gateway}"
        puts "java_home : #{@droplet.java_home.root}"
        puts "v : #{v}"
+       puts "h : #{hostname}"
        puts "debug : #{debug}"
        puts "insecure : #{insecure}"
 
@@ -107,6 +108,9 @@ module JavaBuildpack
        end
        if not_blank?(additional_gateway)
          provision_cmd << " -additional-gateways #{additional_gateway}"
+       end
+       if not_blank?(hostname)
+         provision_cmd << " -h #{hostname}"
        end
        if not_null?(v)
          provision_cmd << " -v "
@@ -170,10 +174,11 @@ module JavaBuildpack
             V                   = 'v'
             DEBUG               = 'debug'
             INSECURE            = 'insecure'
+            H                   = 'h'
 
             private_constant :FILTER, :OMC_URL, :TENANT_ID, :REGKEY, :GATEWAY_HOST, :GATEWAY_PORT,
             :CLASSIFICATIONS, :PROXY_HOST, :PROXY_PORT,  :PROXY_AUTH_TOKEN, :ADDITIONAL_GATEWAY,
-            :AGENT_ZIP_URI, :V, :DEBUG, :INSECURE
+            :AGENT_ZIP_URI, :V, :DEBUG, :INSECURE, :H
 
     end
   end
